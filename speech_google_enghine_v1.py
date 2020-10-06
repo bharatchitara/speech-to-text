e40c1cdb6a7e5362bfb7c17c11eaaa8b66c67869
bharat chitara <bharatchitara99@gmail.com>
2020-10-06 17:53:39 +0530

    updated to v1.2

--- speech_google_enghine_v1.py
+++ speech_google_enghine_v1.py
@@ -34,7 +34,7 @@ while(1):
             # the surrounding noise level
             r.adjust_for_ambient_noise(source2, duration=0.2)
 
-            #listens for the user's input
+            #listens for the user's input using microphone
             audio2 = r.listen(source2)
 
             # Using ggogle to recognize audio
