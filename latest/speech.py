044cdf2f921ec19b58523347ef4e59b954ceb2f9
bharat chitara <bharatchitara99@gmail.com>
2020-10-09 15:43:52 +0530

    capturing output.

--- latest/speech.py
+++ latest/speech.py
@@ -5,17 +5,21 @@ import subprocess
 r = sr.Recognizer()
 with sr.Microphone() as source:
     subprocess.run(["clear"])
+    file =open('output1.txt','w')
     print("-------------------------------------------------------------------------------------------------------------------")
+    print("Speech to text convertion")
     print("-------------------------------------------------------------------------------------------------------------------")
     print("Please wait. Calibrating microphone...")
    # listen for 3 seconds and create the ambient noise energy level
-    r.adjust_for_ambient_noise(source, duration=3)
+    r.adjust_for_ambient_noise(source, duration=2)
     print("Say something!")
     audio = r.listen(source)
 
  # recognize speech using Sphinx
 try:
     print("You said '" + r.recognize_google(audio) + "'")
+    x=r.recognize_google(audio)
+    file.write(x)
 except sr.UnknownValueError:
     print("Engine could not understand audio")
 except sr.RequestError as e:
